6501313e3012af7b260a867326fa3f57c7d7d377
Mazen Alotaibi <mazen.ota@gmail.com>
2019-07-29 15:22:32 -0700

    adding post template and fixed design

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,3 @@
-// Menu button
-$(function() {     
-  $('#menu-toggle').on('click', function(e) {
-    e.preventDefault();
-    $('#menu').slideToggle();
-    $(this).toggleClass('open');
-  });
-});
 // CV page, toggle display for courses list
 var coursesBtn = document.getElementById("courses-btn");
 var coursesList = document.getElementById('courses-list');
@@ -14,22 +6,3 @@ if(coursesBtn != null){
     coursesList.classList.toggle('hide');
   });
 }
-// Goals page, loading bar
-$( document ).ready(function() {
-  $('.loading-bar').each(function(index) {
-    var elem = document.getElementsByClassName("loading-bar")[index]; 
-    elem.style.width = '0%';  
-    elem.innerHTML = '0%';
-    var maxVal = parseInt($(this).attr('class').split(' ')[1]);
-    var widthVal = 1;
-    var id = setInterval(function() {
-      if (widthVal >= maxVal) {
-        clearInterval(id);
-      } else {
-        widthVal++; 
-        elem.style.width = widthVal + '%'; 
-        elem.innerHTML = widthVal * 1 + '%';
-      }
-    }, 10);
-  });
-});
