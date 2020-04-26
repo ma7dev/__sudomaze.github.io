124808ee433330ea0a2f1c1a758ab82938481931
Mazen <alotaima@oregonstate.edu>
2020-04-26 05:46:19 -0700

    testing comments count

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,11 +30,52 @@ function addCopyButtons(clipboard) {
             pre.parentNode.insertBefore(button, pre);
         }
     });
-  }
-  if (navigator && navigator.clipboard) {
+}
+if (navigator && navigator.clipboard) {
     addCopyButtons(navigator.clipboard);
-  } else {
+} else {
     script.onload = function() {
-      addCopyButtons(clipboard);
+        addCopyButtons(clipboard);
     };
-  }
+}
+
+// comments
+(function() { // DON'T EDIT BELOW THIS LINE
+  var d = document, s = d.createElement('script');
+  s.src = 'https://sudomaze.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
+var disqusPublicKey = "{{ site.disqus.public_key}}";
+var disqusShortname = "{{ site.disqus.shortname}}";
+
+$.ajax({
+    type: 'GET',
+    url: 'https://disqus.com/api/3.0/threads/set.json?thread:link=https://sudomaze.dev{{ page.url }}&forum='+disqusShortname+'&api_key='+disqusPublicKey,
+    cache: false,
+    dataType: 'json',
+    success: function (result) {
+    console.log("https://sudomaze.dev");
+    console.log("{{ site.url }}");
+    console.log("https://sudomaze.dev{{ page.url }}");
+    for (var i in result.response) {
+        console.log(result);
+        var count = result.response[i].posts;
+        $('#disqus-comment').html(count);
+    }
+    }
+});
+
+// nav header
+$("#content-btn").click(function() {
+    $("#discussion-btn").removeClass("active");
+    $("#page-discussion").css("display", "none");
+    $("#content-btn").addClass("active");
+    $("#page-content").css("display", "block");
+});
+$("#discussion-btn").click(function() {
+    $("#content-btn").removeClass("active");
+    $("#page-content").css("display", "none");
+    $("#discussion-btn").addClass("active");
+    $("#page-discussion").css("display", "block");
+});
