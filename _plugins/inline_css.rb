d638b9eae7490e07fc39b21292955a7d5e55c49b
Mazen <alotaima@oregonstate.edu>
2020-09-09 11:38:09 -0700

    github pages issue with custom plugins

--- _plugins/inline_css.rb
+++ _plugins/inline_css.rb
@@ -1,16 +1,3 @@
-# Usage:
-# This adds a tag by the name `inline_css`. It takes one argument which is the
-# path to a CSS file without any quotes:
-#
-# {% inline_css public/css/atf.css %}
-#
-# This would then read the file in the given directory relative to the root of
-# the Jekyll installation and then embed that CSS at that location.
-#
-# Installation:
-#
-# Copy this file and place it under _plugins directory. Naming of the file
-# doesn't matter
 module Jekyll
   class InlineCssTag < Liquid::Tag
     def initialize(tag_name, path_to_atf_css, tokens)
